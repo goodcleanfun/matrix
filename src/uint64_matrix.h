e33b02ae60b19b7fc176b68f62e4af837bfee31f
Al <albarrentine@gmail.com>
2024-01-22 00:48:23 -0500

    [uint64] adding uint64_matrix

---
+++ src/uint64_matrix.h
@@ -0,0 +1,11 @@
+#ifndef UINT64_MATRIX_H
+#define UINT64_MATRIX_H
+
+#define MATRIX_NAME uint64_matrix
+#define MATRIX_TYPE uint64_t
+#include "num_arrays/uint64_array.h"
+#include "matrix.h"
+#undef MATRIX_NAME
+#undef MATRIX_TYPE
+
+#endif
